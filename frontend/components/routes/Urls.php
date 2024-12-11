4a54d93ae4c4e6cb0705b5f87c13bda0ba081523
alexkriak <lemon1and3cherry@gmail.com>
2024-12-11 17:43:46 +0400

    add search document in

---
+++ frontend/components/routes/Urls.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace frontend\components\routes;
+
+class Urls
+{
+    const DOC_IN_VIEW = "document/document-in/view";
+    const DOC_IN_CREATE = "document/document-in/create";
+    const DOC_IN_RESERVE = "document/document-in/reserve";
+    const DOC_IN_INDEX = "document/document-in/index";
+}
